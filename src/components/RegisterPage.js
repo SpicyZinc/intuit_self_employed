b81036a740cb87b7dbfb1084dfec4b0608361dab
HackerRank <hackerrank@example.com>
2019-01-06 06:24:25 +0000

    Saved at Sat Jan 05 2019 22:24:24 GMT-0800 (Pacific Standard Time)

--- src/components/RegisterPage.js
+++ src/components/RegisterPage.js
@@ -32,7 +32,7 @@ export class RegisterPage extends Component {
         // handle button click and dispatch register
         event.preventDefault();
         
-        this.setState
+        this.setState();
     }
 
     render() {
