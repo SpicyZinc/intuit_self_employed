ff9ea45de876c0a72ffa0444ec87801bae2c4f05
HackerRank <hackerrank@example.com>
2019-01-06 05:19:57 +0000

    Saved at Sat Jan 05 2019 21:19:57 GMT-0800 (Pacific Standard Time)

--- src/actions/user.actions.js
+++ src/actions/user.actions.js
@@ -12,7 +12,7 @@ export const userActions = {
 function login(username, password) {
     // return the promise using fetch which adds to localstorage on resolve
     return (dispatch) => {
-        dispatch(request({}))
+        dispatch(request({u}))
     };
 
     function request(user) { return { type: userConstants.LOGIN_REQUEST, user } }
