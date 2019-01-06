c54656ef2ad4a15641f2788dae3c58db58e30862
HackerRank <hackerrank@example.com>
2019-01-06 06:23:08 +0000

    Saved at Sat Jan 05 2019 22:23:08 GMT-0800 (Pacific Standard Time)

--- src/components/RegisterPage.js
+++ src/components/RegisterPage.js
@@ -24,7 +24,7 @@ export class RegisterPage extends Component {
     handleChange(event) {
         // handle input change and dispatch register
         this.setState({
-            username: 
+            username: event.ta
         });
     }
 
