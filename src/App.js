e4fde540d7a7a25a9fd5fdb17fd26bbe1f8d8112
HackerRank <hackerrank@example.com>
2019-01-06 04:27:43 +0000

    Saved at Sat Jan 05 2019 20:27:43 GMT-0800 (Pacific Standard Time)

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import {
     Router,
     Route,
     Link,
-    Nav
+    NavLin
 } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { PrivateRoute } from './PrivateRoute.js';
