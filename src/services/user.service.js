522b020a5245c3d4aeda52495121aabfa243cad9
HackerRank <hackerrank@example.com>
2019-01-06 05:31:08 +0000

    Saved at Sat Jan 05 2019 21:31:07 GMT-0800 (Pacific Standard Time)

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -13,7 +13,7 @@ function login(username, password) {
 
     // call `/users/authenticate` with requestOptions to authenticate the login process
     return fetch('/users/authenticate')
-            .then()
+            .then();
     
 }
 
