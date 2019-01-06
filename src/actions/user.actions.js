7eefed738e67c174236a3cc10ad721302616e25e
HackerRank <hackerrank@example.com>
2019-01-06 05:58:11 +0000

    Saved at Sat Jan 05 2019 21:58:10 GMT-0800 (Pacific Standard Time)

--- src/actions/user.actions.js
+++ src/actions/user.actions.js
@@ -39,7 +39,18 @@ function logout() {
 
 function register(user) {
     // return the promise using fetch which dispatches appropriately
-    
+        return (dispatch) => {
+        dispatch(request({username}));
+        
+        userService.login(username, password)
+            .then((user) => {
+                dispatch(success(user));
+                history.push('/');
+            })
+            .catch((error) => {
+                dispatch(failure(error.toString()));
+            });
+    };
 
     function request(user) { return { type: userConstants.REGISTER_REQUEST, user } }
     function success(user) { return { type: userConstants.REGISTER_SUCCESS, user } }
