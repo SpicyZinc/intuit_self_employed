700eaff2d1bc433cd3b938841fcf2f81ff7989ef
HackerRank <hackerrank@example.com>
2019-01-06 05:12:52 +0000

    Saved at Sat Jan 05 2019 21:12:52 GMT-0800 (Pacific Standard Time)

--- src/actions/user.actions.js
+++ src/actions/user.actions.js
@@ -11,7 +11,7 @@ export const userActions = {
 
 function login(username, password) {
     // return the promise using fetch which adds to localstorage on resolve
-    return () => {
+    return (d) => {
         
     };
 
