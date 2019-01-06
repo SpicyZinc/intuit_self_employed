19c326a7a70f34eef51e7592e3047c839812fbf4
HackerRank <hackerrank@example.com>
2019-01-06 06:28:29 +0000

    Saved at Sat Jan 05 2019 22:28:29 GMT-0800 (Pacific Standard Time)

--- src/components/RegisterPage.js
+++ src/components/RegisterPage.js
@@ -73,7 +73,7 @@ export class RegisterPage extends Component {
 
 // complete the below function
 function mapStateToProps(state) {
-        
+    
 }
 
 export { RegisterPage as TestRegisterPage };
