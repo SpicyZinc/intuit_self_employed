3a8f62b14dd897352c0b3fbc6348ee9e6745bd49
HackerRank <hackerrank@example.com>
2019-01-06 16:44:14 +0000

    Saved at Sun Jan 06 2019 08:44:15 GMT-0800 (Pacific Standard Time)

--- src/App.js
+++ src/App.js
@@ -29,9 +29,7 @@ export class App extends React.Component {
         return (
             <div className="container">
                 <div className="col-sm-8 col-sm-offset-2">
-                    // {
-                        alert.message && <div className={`alert ${alert.type}`}>{alert.message}</div>
-                    }
+ 
                     
                     <Router history={history}>
                         <div>
