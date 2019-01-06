6943146cfd3bddc166c1761102f1337d9b78a197
HackerRank <hackerrank@example.com>
2019-01-06 16:40:10 +0000

    Saved at Sun Jan 06 2019 08:40:10 GMT-0800 (Pacific Standard Time)

--- src/App.js
+++ src/App.js
@@ -33,7 +33,7 @@ export class App extends React.Component {
                         alert.message && <div className={`alert ${alert.type}`}>{alert.message}</div>
                     }
                     
-                    <Router history=>
+                    <Router history={}>
                         
                     </Router>
                 </div>
