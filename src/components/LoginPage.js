87daf64eafc0160aad9cb2c4e6e7ca9e9c4180bc
HackerRank <hackerrank@example.com>
2019-01-06 04:34:02 +0000

    Saved at Sat Jan 05 2019 20:34:01 GMT-0800 (Pacific Standard Time)

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -50,7 +50,7 @@ export class LoginPage extends Component {
                     </div>
                     <div className="form-group">
                         <button className="btn btn-primary">Login</button>
-                        <button className="btn btn-primary">Regis</button>
+                        <button className="btn btn-primary">Register</button>
                     </div>
                 </form>
             </div>
