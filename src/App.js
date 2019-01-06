95336c711560d6dac4cb4fdd1f5d97586af3b80b
HackerRank <hackerrank@example.com>
2019-01-06 16:41:12 +0000

    Saved at Sun Jan 06 2019 08:41:13 GMT-0800 (Pacific Standard Time)

--- src/App.js
+++ src/App.js
@@ -35,7 +35,7 @@ export class App extends React.Component {
                     
                     <Router history={history}>
                         <div>
-                            <PrivateRoute exact path=''>
+                            <PrivateRoute exact path='/'>
                         </div>
                     </Router>
                 </div>
