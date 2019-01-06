5907daaeba88ed4daa990271a92656b6efd23662
HackerRank <hackerrank@example.com>
2019-01-06 05:20:11 +0000

    Saved at Sat Jan 05 2019 21:20:11 GMT-0800 (Pacific Standard Time)

--- src/actions/user.actions.js
+++ src/actions/user.actions.js
@@ -14,7 +14,7 @@ function login(username, password) {
     return (dispatch) => {
         dispatch(request({username}));
         
-        userService
+        userService.log()
     };
 
     function request(user) { return { type: userConstants.LOGIN_REQUEST, user } }
