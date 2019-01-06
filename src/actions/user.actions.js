3489ed5e7cd62319b232786e8eba5ca4194ee7a1
HackerRank <hackerrank@example.com>
2019-01-06 05:58:03 +0000

    Saved at Sat Jan 05 2019 21:58:03 GMT-0800 (Pacific Standard Time)

--- src/actions/user.actions.js
+++ src/actions/user.actions.js
@@ -39,6 +39,7 @@ function logout() {
 
 function register(user) {
     // return the promise using fetch which dispatches appropriately
+    
 
     function request(user) { return { type: userConstants.REGISTER_REQUEST, user } }
     function success(user) { return { type: userConstants.REGISTER_SUCCESS, user } }
